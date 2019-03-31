d42e92057694528f6f57785629a4f7cb23ed6844
Harry Dbrandy <devdbrandy@gmail.com>
2019-03-31 19:50:07 +0100

    chore(setup): install required dependencies + config webpack

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 5000,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: 'public/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  resolve: { extensions: ['.js', '.jsx'] },
+};
