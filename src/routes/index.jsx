678a4da133ee2a5564cda5968210a58acdba79e3
Harry Dbrandy <devdbrandy@gmail.com>
2019-04-12 18:22:14 +0100

    chore(setup): provide linting config and template structure

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '../components/container/Home';
+import Signin from '../components/container/SigninPage';
 import About from '../components/view/AboutPage';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/signin" component={Signin} />
       <Route path="/about" component={About} />
     </Switch>
   </Router>
