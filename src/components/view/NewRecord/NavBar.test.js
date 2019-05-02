d77e06307d687515eb813f8afff4f5118c26111a
Harry Dbrandy <devdbrandy@gmail.com>
2019-05-02 11:52:59 +0100

    test(NavBar): :white_check_mark: add component test

---
+++ src/components/view/NewRecord/NavBar.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { NewRecord } from './index';
+
+/**
+ * Wrapper for enzyme shallow component
+ *
+ * @returns {object} The mocked props and component
+ */
+function setup() {
+  const props = {
+    createRecord: jest.fn(),
+  };
+
+  const component = shallow(<NewRecord {...props} />);
+  return { props, component };
+}
+
+describe('<NewRecord />', () => {
+  it('renders without crashing given the required props', () => {
+    const { component } = setup();
+    expect(component).toMatchSnapshot();
+  });
+});
